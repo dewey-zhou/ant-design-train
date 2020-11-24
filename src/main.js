9712bc041b260cb3b974dd83be0fccc728a8a009
dewey <1317336077@qq.com>
2020-11-24 11:00:31 +0800

    配置webpack和Babel

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+// import Anted from "ant-design-vue";
+// import Button from "ant-design-vue/lib/button";
+import { Button } from "ant-design-vue";
+// import "ant-design-vue/dist/antd.less";
+// import "ant-design-vue/lib/button/style";
 Vue.config.productionTip = false;
 
+Vue.use(Button);
+// Vue.use(Anted);
 new Vue({
   router,
   store,
